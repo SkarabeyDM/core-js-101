347be11aeb42ab52b1a44a7b52f81614985d4e88
SkarabeyDM <mechanoid0415@gmail.com>
2023-06-01 20:38:49 +0300

    feat: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,14 @@
 module.exports = {
   env: {
+    browser: true,
     commonjs: true,
-    es6: true,
-    node: true,
-    mocha: true,
+    es2021: true,
   },
-  extends: [
-    'airbnb-base',
+  extends: 'airbnb-base',
+  overrides: [
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 'latest',
   },
   rules: {
   },
